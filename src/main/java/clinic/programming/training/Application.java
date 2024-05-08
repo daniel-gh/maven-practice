60eea645935530e90d3650eb5330c27888c637ac
danielly <danielly@tw.ibm.com>
2024-05-08 15:43:11 +0800

    initial commit

---
+++ src/main/java/clinic/programming/training/Application.java
@@ -0,0 +1,25 @@
+package clinic.programming.training;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+
+public class Application {
+    
+	public int countWords(String words) {
+		String[] separateWords = StringUtils.split(words, ' ');
+		return (separateWords == null) ? 0 : separateWords.length;
+	}
+	
+    public Application() {
+        System.out.println ("Inside Application");
+    }
+
+    // method main(): ALWAYS the APPLICATION entry point
+    public static void main (String[] args) {
+    	System.out.println ("Starting Application");
+		Application app = new Application();
+		
+		app.countWords("Hello World Maven");
+    }
+}
